a464bf2f8ff3b9a4fa722345b4406d7917e3b04c
ahmed-elwakeel <ahmed.o.elwakeel@gmail.com>
2024-01-02 14:17:35 +0100

    Initial commit

---
+++ storage/innobase/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ahmedelwakeel/mysql-5.7.24/storage/innobase
+# Build directory: /home/ahmedelwakeel/mysql-5.7.24/storage/innobase
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
